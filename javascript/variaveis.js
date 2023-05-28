2dd9f676d9f1152d35c7b1cee9ea0d04b312dcc0
GuiiKal <85639009+GuiiKal@users.noreply.github.com>
2023-05-28 18:47:00 -0300

    iniciando js

---
+++ javascript/variaveis.js
@@ -0,0 +1,17 @@
+// Variáveis
+
+let idade = 20;
+console.log(idade);
+
+// Tipos de variáveis
+let numero = 21; //number
+let altura = 1.77; //number
+let nome = 'Guilherme'; //string
+let estudando = true; // boolean (true or false)
+
+console.log(nome, typeof nome); //typeof imprime o tipo da variável
+
+let outra;
+console.log(outra); 
+
+const constante = 20; //const não pode alterar o valor
