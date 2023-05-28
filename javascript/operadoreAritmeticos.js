24de50d96b75750e16904e5b3e485d3732f51bc9
GuiiKal <85639009+GuiiKal@users.noreply.github.com>
2023-05-28 19:24:15 -0300

    aritmeticos e booleanos js

---
+++ javascript/operadoreAritmeticos.js
@@ -0,0 +1,28 @@
+// Operadores aritméticos
+
+let number1 = 20;
+let number2 = 2;
+
+// adição
+console.log('Soma = ', number1 + number2);
+const soma = number1+ number2;
+console.log(soma);
+
+//subtração
+console.log('Subtração = ', number1 - number2);
+//multiplicação
+console.log('Multiplicação = ', number1 * number2);
+//divisão
+console.log('Divisão = ', number1 / number2);
+//exponenciação
+console.log('Exponenciação = ', number1 ** number2);
+//resto da divisão
+console.log('Resto da divisão = ', number1 % number2);
+
+// Precedência de operadores
+console.log(3 + 7 * 3) //segue as regras normais da matemática
+console.log((3 + 7) * 3)
+
+// Biblioteca Match
+console.log(Math.PI);
+console.log(Math.sqrt(16))
---
+++ javascript/operadoresBooleanos.js
@@ -0,0 +1,25 @@
+// Operadores Booleanos
+
+//Igualdade: == (ou ===)
+//Desigualdade: != (ou !===)
+//Maior que: >
+//Menor que: <
+//Maior ou igual: >=
+//Menor ou igual: <=
+
+//Conjuntos Lógicos
+
+//AND => &&
+//OR => ||
+//NOT (INVERTE AS INFORMAÇÕES) => ! 
+
+const num = 10; 
+console.log(num > 20)
+console.log(num == 20)
+
+console.log(num == 10)
+console.log(num == 10) //aqui ele compompara somente o conteúdo 
+console.log(num === '10') //aqui ele compara o conteúdo e o tipo da variável 
+
+console.log(num != 10) //aqui ele compompara somente o conteúdo 
+console.log(num !== '10') //aqui ele compara o conteúdo e o tipo da variável 
