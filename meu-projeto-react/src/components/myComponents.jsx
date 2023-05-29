51f625c42a513aba0e12fa3266c8d4acc2a8b096
GuiiKal <85639009+GuiiKal@users.noreply.github.com>
2023-05-28 22:35:51 -0300

    hello word react

---
+++ meu-projeto-react/src/components/myComponents.jsx
@@ -0,0 +1,7 @@
+export default function MeuComponente() {
+    return (
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid, quisquam recusandae!
+        Dolor dignissimos atque quidem doloremque optio veniam reiciendis maxime consequatur doloribus deserunt eos aliquam commodi,
+        vitae soluta dolores praesentium.</p>
+    )
+  }
